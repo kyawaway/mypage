50075131b1bb258cbf2c27ead93df0f9fcae9879
kyawaway <tkmz0216@gmail.com>
2026-01-30 00:30:18 +0900

    convert path

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 npm run build
+
+echo "Converting absolute paths to relative paths..."
+
+sed -i '' 's|href="/|href="./|g' dist/index.html
+sed -i '' 's|src="/|src="./|g' dist/index.html
+
+sed -i '' 's|href="/|href="../|g' dist/publication-list/index.html
+sed -i '' 's|src="/|src="../|g' dist/publication-list/index.html
+
+echo "Done!"
