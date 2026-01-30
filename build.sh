c2d96b4ea7c2b4a943cfe563d16bac61da2ef606
kyawaway <tkmz0216@gmail.com>
2026-01-30 10:22:32 +0900

    update

--- build.sh
+++ build.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-npm run build
+BASE_PATH="${1:-}"
 
-echo "Converting absolute paths to relative paths..."
-
-sed -i '' 's|href="/|href="./|g' dist/index.html
-sed -i '' 's|src="/|src="./|g' dist/index.html
-
-sed -i '' 's|href="/|href="../|g' dist/publication-list/index.html
-sed -i '' 's|src="/|src="../|g' dist/publication-list/index.html
-
-echo "Done!"
+npm run build --base="$BASE_PATH" 
